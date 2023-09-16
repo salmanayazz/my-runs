2302382aa8d64b5746706c60b122d3eb9a2f2b71
Salman Ayaz <salmanayazz@gmail.com>
2023-09-16 14:57:51 -0700

    updated gitignore, cleaned up files

--- app/src/main/java/com/example/myruns/SettingsActivity.kt
+++ app/src/main/java/com/example/myruns/SettingsActivity.kt
@@ -9,39 +9,44 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.os.Environment
 import android.provider.MediaStore
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.FileProvider
 import java.io.File
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+
 
 class SettingsActivity : AppCompatActivity() {
-    private val cameraRequestCode: Int = 1;
-    private lateinit var imageCaptureLauncher: ActivityResultLauncher<Intent>
+    private val cameraRequestCode: Int = 1
+    private val storageRequestCode: Int = 2
+
+    private val imageCaptureLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) { // successfully captured photo
+            val data: Intent? = result.data
+            if (data != null && data.extras != null) {
+                val imageBitmap = data.extras!!.get("data") as Bitmap // TODO: change to something not deprecated
+                setProfileImage(imageBitmap)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        imageCaptureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = result.data
-                if (data != null && data.extras != null) {
-                    val imageBitmap = data.extras!!.get("data") as Bitmap
-                    setProfileImage(imageBitmap)
-                }
-            }
-        }
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), storageRequestCode)
 
-        val imageButton: Button = findViewById(R.id.profile_photo_button)
-        imageButton.setOnClickListener {
-            // show popup to ask for camera permissions
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), cameraRequestCode)
+        findViewById<Button>(R.id.profile_photo_button).setOnClickListener {
+
+            // show popup to ask for camera permissions, if successful, launch camera
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                cameraRequestCode
+            )
         }
     }
 
@@ -56,19 +61,52 @@ class SettingsActivity : AppCompatActivity() {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) { // granted
                 launchCamera()
             } else { // denied
-                val toast = Toast.makeText(this, "Please enable camera permissions", Toast.LENGTH_SHORT)
-                toast.show()
+                Toast.makeText(
+                    this,
+                    "Please enable camera permissions",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else if (requestCode == storageRequestCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(
+                    this,
+                    "Storage Permission Granted",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Storage Permission Denied",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
 
     private fun launchCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        imageCaptureLauncher.launch(intent)
+
+        try {
+            imageCaptureLauncher.launch(intent)
+        } catch (e: Exception) {
+            e.printStackTrace();
+        }
     }
 
     private fun setProfileImage(image: Bitmap) {
         var profilePhoto: ImageView = findViewById(R.id.profile_photo)
         profilePhoto.setImageBitmap(image)
+
+        // save the image
+        val imageFileName = "profile_image.jpg"
+        val imageFile = File(getExternalFilesDir(null), imageFileName)
+//
+//        val tempImgFile = File(getExternalFilesDir(null), "profile_image.jpg")
+//        val tempImgUri = FileProvider.getUriForFile(this, "com.example.myruns", tempImgFile)
+//
+//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, tempImgUri)
+//        cameraResult.launch(intent)
     }
 }
