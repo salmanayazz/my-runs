91335b3e82fd05c2eb872f416079750254d67292
Salman Ayaz <salmanayazz@gmail.com>
2023-10-01 12:33:32 -0700

    styled TabLayout fragment

--- app/src/main/java/com/example/salman_ayaz_myruns/MainActivity.kt
+++ app/src/main/java/com/example/salman_ayaz_myruns/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.salman_ayaz_myruns
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var tabFragments = ArrayList<Fragment>()
+    private val startRunsFragment = StartRunsFragment()
+    private val historyFragment = HistoryFragment()
+    private val settingsFragment = SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
 
-        val settings = Intent(this, ProfileActivity::class.java);
-        startActivity(settings);
+        tabFragments.add(startRunsFragment)
+        tabFragments.add(historyFragment)
+        tabFragments.add(settingsFragment)
+
     }
 }
