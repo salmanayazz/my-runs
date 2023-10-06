8611b161165f158d84681d9bba5ec79affd1e434
Salman Ayaz <salmanayazz@gmail.com>
2023-10-06 14:51:20 -0700

    added dropdown menu for runs input type

--- app/src/main/java/com/example/salman_ayaz_myruns/StartRunsFragment.kt
+++ app/src/main/java/com/example/salman_ayaz_myruns/StartRunsFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Spinner
+import com.google.android.material.textfield.TextInputLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +38,14 @@ class StartRunsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start_runs, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_start_runs, container, false)
+
+        setupACTV(
+            rootView,
+            R.id.actv_input_type,
+            listOf("Manual Entry", "GPS", "Automatic")
+        )
+        return rootView
     }
 
     companion object {
@@ -55,5 +66,16 @@ class StartRunsFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun setupACTV(rootView: View, actvID: Int, menuOptions: List<String>) {
+        val autoCompleteTextView = rootView.findViewById<AutoCompleteTextView>(actvID)
+
+        // create an ArrayAdapter with the options and a simple layout for dropdown items
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, menuOptions)
+        autoCompleteTextView.setAdapter(adapter)
+
+        // set default value to be the first element
+        autoCompleteTextView.setText(menuOptions[0], false)
     }
 }
