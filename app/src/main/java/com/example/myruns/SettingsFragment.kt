9537c6289263e8f388eb817cdac4f54bf6991fbb
Salman Ayaz <salmanayazz@gmail.com>
2023-10-09 01:06:12 -0700

    removed hardcoded application id in link to ProfileActivity

--- app/src/main/java/com/example/myruns/SettingsFragment.kt
+++ app/src/main/java/com/example/myruns/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.myruns
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 /**
@@ -14,11 +16,15 @@ import androidx.preference.PreferenceFragmentCompat
  */
 class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference, rootKey)
+
+        // click listener that opens ProfileActivity
+        val userProfilePreference = findPreference<Preference>("user_profile")
+        userProfilePreference?.setOnPreferenceClickListener {
+            val intent = Intent(requireContext(), ProfileActivity::class.java)
+            startActivity(intent)
+            true
+        }
     }
 }
