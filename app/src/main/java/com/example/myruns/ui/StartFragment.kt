ec8c8021476e1cdd6dba24b71fe31f7ce9366c10
Salman Ayaz <salmanayazz@gmail.com>
2023-11-22 22:32:09 -0800

    fixed issue causing dropdown to lose its options

--- app/src/main/java/com/example/myruns/ui/StartFragment.kt
+++ app/src/main/java/com/example/myruns/ui/StartFragment.kt
@@ -38,48 +38,50 @@ class StartFragment : Fragment() {
     private lateinit var inputType: AutoCompleteTextView
     private lateinit var activityType: AutoCompleteTextView
     private lateinit var startButton: Button
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var rootView: View? = null
+    private var selectedInput = 0
+    private var selectedActivity = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val rootView = inflater.inflate(R.layout.fragment_start, container, false)
+        rootView = inflater.inflate(R.layout.fragment_start, container, false)
+        return rootView
+    }
 
-
+    private fun setup() {
+        if (rootView == null) { return }
         // setup AutoCompleteTextViews
-        inputType = rootView.findViewById(R.id.actv_input_type)
-        activityType = rootView.findViewById(R.id.actv_activity_type)
+        inputType = rootView!!.findViewById(R.id.actv_input_type)
+        activityType = rootView!!.findViewById(R.id.actv_activity_type)
 
         setupACTV(
-            rootView,
+            rootView!!,
             inputType.id,
-            inputTypeList
+            inputTypeList,
+            inputTypeList[selectedInput]
         )
 
         setupACTV(
-            rootView,
+            rootView!!,
             activityType.id,
-            activityTypeList
+            activityTypeList,
+            activityTypeList[selectedActivity]
         )
 
         // listeners for AutoCompleteTextViews
-        var selectedInput = 0
         inputType.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
             selectedInput = position
         }
 
-        var selectedActivity = 0
         activityType.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
             selectedActivity = position
         }
 
         // submit and start ManualInputActivity or MapDisplayActivity
-        startButton = rootView.findViewById(R.id.button_start)
+        startButton = rootView!!.findViewById(R.id.button_start)
         startButton.setOnClickListener() {
             // start activity based on user selection
             val intent = if (inputTypeList[selectedInput] == INPUT_TYPE_MANUAL) {
@@ -92,8 +94,6 @@ class StartFragment : Fragment() {
             }
             startActivity(intent)
         }
-
-        return rootView
     }
 
     /**
@@ -105,7 +105,7 @@ class StartFragment : Fragment() {
      * @param menuOptions
      * the list of options to display in the AutoCompleteTextView
      */
-    private fun setupACTV(rootView: View, actvID: Int, menuOptions: List<String>) {
+    private fun setupACTV(rootView: View, actvID: Int, menuOptions: List<String>, text: String) {
         val autoCompleteTextView = rootView.findViewById<AutoCompleteTextView>(actvID)
 
         // create an ArrayAdapter with the options and a simple layout for dropdown items
@@ -113,6 +113,12 @@ class StartFragment : Fragment() {
         autoCompleteTextView.setAdapter(adapter)
 
         // set default value to be the first element
-        autoCompleteTextView.setText(menuOptions[0], false)
+        autoCompleteTextView.setText(text, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        setup()
     }
 }
