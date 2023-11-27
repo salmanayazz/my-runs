3b33933d928f38d59dcdd7559f1f83e248c98793
Salman Ayaz <salmanayazz@gmail.com>
2023-11-27 01:21:54 -0800

    integrated activity detection with FFT algo

--- app/src/main/java/com/example/myruns/TrackingService.kt
+++ app/src/main/java/com/example/myruns/TrackingService.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
@@ -28,10 +29,17 @@ import androidx.core.content.ContextCompat
 import com.example.myruns.ui.StartFragment
 import com.example.myruns.ui.mapdisplay.MapDisplayActivity
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlin.math.abs
 import kotlin.math.sqrt
+import weka.core.DenseInstance
+import weka.core.Instance
+import java.util.LinkedList
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.CopyOnWriteArrayList
 
 
 class TrackingService : Service(), LocationListener, SensorEventListener {
@@ -44,6 +52,8 @@ class TrackingService : Service(), LocationListener, SensorEventListener {
 
         val DETECT_ACTIVITY = "detect-activity"
     }
+    
+    private val ACCELEROMETER_BLOCK_CAPACITY = 64
 
     private var messageHandler: Handler? = null
     private val CHANNEL_ID = "Tracking Notification"
@@ -53,7 +63,10 @@ class TrackingService : Service(), LocationListener, SensorEventListener {
     private val notificationManager by lazy { getSystemService(NOTIFICATION_SERVICE) as NotificationManager }
     private val trackingBinder by lazy { TrackingBinder() }
     private var sensorManager: SensorManager? = null
-    private val magnitude = ArrayList<Any?>()
+    private val sensorReadingBuffer = mutableListOf<Double>()
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
+    private val fft = FFT(ACCELEROMETER_BLOCK_CAPACITY)
+    private val bufferLock = Any()
 
     override fun onCreate() {
         checkPermission()
@@ -65,9 +78,6 @@ class TrackingService : Service(), LocationListener, SensorEventListener {
         checkPermission()
         startTracking()
 
-        if (intent != null) {
-            println("start activity detect: ${intent.getBooleanExtra(DETECT_ACTIVITY, false)}")
-        }
         if (intent != null && intent.getBooleanExtra(DETECT_ACTIVITY, false)) {
             startActivityDetection()
         }
@@ -86,7 +96,6 @@ class TrackingService : Service(), LocationListener, SensorEventListener {
 
     inner class TrackingBinder : Binder() {
         fun setMessageHandler(messageHandler: Handler) {
-            Log.i("Tracking Service", "setMessageHandler has been called")
             this@TrackingService.messageHandler = messageHandler
         }
     }
@@ -163,10 +172,14 @@ class TrackingService : Service(), LocationListener, SensorEventListener {
         }
     }
 
+    /**
+     * starts detecting the user's activity
+     */
     private fun startActivityDetection() {
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        val sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        val sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+        processSensorReadingBuffer()
     }
 
     /**
@@ -194,33 +207,90 @@ class TrackingService : Service(), LocationListener, SensorEventListener {
         }
     }
 
+
     override fun onSensorChanged(event: SensorEvent) {
-        CoroutineScope(IO).launch {
-            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+        val x = event.values[0].toDouble()
+        val y = event.values[1].toDouble()
+        val z = event.values[2].toDouble()
 
-                val x = event.values[0].toDouble()
-                val y = event.values[1].toDouble()
-                val z = event.values[2].toDouble()
+        // add the magnitude to the buffer
+        synchronized(bufferLock) {
+            sensorReadingBuffer.add(sqrt(x * x + y * y + z * z))
 
-                // keep arraylist size as 65
-                if (magnitude.size > 64) {
-                    magnitude.removeFirst()
-                }
-
-                magnitude.add(sqrt(x * x + y * y + z * z))
-
-                try {
-                    val detectedVal = WekaClassifier.classify(magnitude.toArray())
-                    val activityType = abs(detectedVal.toInt() - 2)
-                    Log.i("onSensorChanged", "activity: ${
-                        StartFragment.activityTypeList[activityType]
-                    }")
-                    sendActivity(abs(activityType))
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
+            // keep buffer size at ACCELEROMETER_BLOCK_CAPACITY
+            if (sensorReadingBuffer.size > ACCELEROMETER_BLOCK_CAPACITY) {
+                sensorReadingBuffer.removeAt(0)
             }
         }
+    }
+
+    /**
+     * processes the sensor reading buffer and classifies the activity
+     */
+    private fun processSensorReadingBuffer() {
+        Thread {
+            while (true) {
+                // check if there are enough values in the buffer
+                val values: List<Double>
+                synchronized(bufferLock) {
+                    values = if (sensorReadingBuffer.size == ACCELEROMETER_BLOCK_CAPACITY) {
+                        sensorReadingBuffer.toList()
+                    } else {
+                        emptyList()
+                    }
+                }
+
+                if (values.isNotEmpty()) {
+                    val featureVector = generateFeatureArray(values)
+                    try {
+                        val detectedVal = WekaClassifier.classify(featureVector)
+                        val activityType = abs(detectedVal - 2).toInt()
+                        Log.i(TAG, "activity: ${StartFragment.activityTypeList[activityType]}")
+                        sendActivity(activityType)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                    // Clear the buffer after processing
+                    synchronized(bufferLock) {
+                        sensorReadingBuffer.clear()
+                    }
+                }
+            }
+        }.start()
+    }
+
+    /**
+     * generates a feature array from the sensor reading buffer
+     * @param magnitude
+     * the magnitude of the sensor readings
+     * @return
+     * the feature array
+     */
+    private fun generateFeatureArray(magnitude: List<Double>): Array<Any?> {
+        val realNums = DoubleArray(ACCELEROMETER_BLOCK_CAPACITY)
+        val imaginaryNums = DoubleArray(ACCELEROMETER_BLOCK_CAPACITY)
+
+        val max = magnitude.maxOrNull() ?: 0.0
+
+        for (i in realNums.indices) {
+            realNums[i] = magnitude[i]
+            imaginaryNums[i] = 0.0
+        }
+
+        fft.fft(realNums, imaginaryNums)
+
+        val featureVector = arrayOfNulls<Any>(ACCELEROMETER_BLOCK_CAPACITY + 1)
+
+        for (i in realNums.indices) {
+            val mag = sqrt(realNums[i] * realNums[i] + imaginaryNums[i] * imaginaryNums[i])
+            featureVector[i] = mag
+            imaginaryNums[i] = 0.0
+        }
+
+        featureVector[ACCELEROMETER_BLOCK_CAPACITY] = max
+
+        return featureVector
     }
 
     private fun sendActivity(activity: Int) {
@@ -244,6 +314,7 @@ class TrackingService : Service(), LocationListener, SensorEventListener {
         sensorManager?.unregisterListener(this)
 
         notificationManager.cancel(NOTIFY_ID)
+        coroutineScope.cancel()
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
